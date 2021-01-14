b37062dbcc9fbc18df10877be332d5336c7ad5e5
Pablo Wolff <pablowolff@Pablos-MacBook-Pro.local>
2021-01-14 08:24:15 -0500

    created map activity

---
+++ android/app/src/main/java/com/mapdemo/MapDemo.java
@@ -0,0 +1,68 @@
+package com.mapdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import android.util.Log;
+import com.here.sdk.mapview.MapView;
+import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapScene;
+import com.here.sdk.mapview.MapScheme;
+
+import android.os.Bundle;
+
+public class MapDemo extends AppCompatActivity {
+    private static final String TAG = MapDemo.class.getSimpleName();
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_demo);
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.setOnReadyListener(new MapView.OnReadyListener() {
+            @Override
+            public void onMapViewReady() {
+                // This will be called each time after this activity is resumed.
+                // It will not be called before the first map scene was loaded.
+                // Any code that requires map data may not work as expected beforehand.
+                Log.d(TAG, "HERE Rendering Engine attached.");
+            }
+        });
+    }
+
+    private void loadMapScene() {
+        // Load a scene from the HERE SDK to render the map with a map scheme.
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, new MapScene.LoadSceneCallback() {
+            @Override
+            public void onLoadScene(@Nullable MapError mapError) {
+                if (mapError == null) {
+                    double distanceInMeters = 1000 * 10;
+                    mapView.getCamera().lookAt(
+                            new GeoCoordinates(52.530932, 13.384915), distanceInMeters);
+                } else {
+                    Log.d(TAG, "Loading map failed: mapError: " + mapError.name());
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
